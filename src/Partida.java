5a319fa900d27a8cd87e799d316328d45955545d
Alamo Pereira Saravali <alamo.saravali@gmail.com>
2014-02-04 20:08:59 -0200

    Initial commit to Partida class

---
+++ src/Partida.java
@@ -0,0 +1,18 @@
+
+public class Partida extends Thread
+{
+	Player p1;
+	Player p2;
+	
+	public Partida(Player p1, Player p2)
+	{
+		this.p1 = p1;
+		this.p2 = p2;
+	}
+	
+	@Override
+	public void run()
+	{
+		
+	}
+}
