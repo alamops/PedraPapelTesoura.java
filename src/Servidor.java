1f3106deaa08b7ed858bf3e042e440833469a114
Alamo Pereira Saravali <alamo.saravali@gmail.com>
2014-02-06 00:06:03 -0200

    put while into try

--- src/Servidor.java
+++ src/Servidor.java
@@ -63,53 +63,53 @@ public class Servidor
 		try
 		{
 			serverSocket = new ServerSocket(5000);
+			
+			while(contPlayers < NUM_MAX_PLAYERS)
+			{
+				try
+				{
+					//abre socket e streams de entrada e saida
+					Socket novaConexao = serverSocket.accept();
+					DataInputStream stream_entrada = new DataInputStream(novaConexao.getInputStream());
+					DataOutputStream stream_saida = new DataOutputStream(novaConexao.getOutputStream());
+					ObjectInputStream object_entrada = new ObjectInputStream(novaConexao.getInputStream());
+					ObjectOutputStream object_saida = new ObjectOutputStream(novaConexao.getOutputStream());
+					
+					//Pedir Nome
+					String nome = stream_entrada.readUTF();
+					
+					//Cria objeto Jogador
+					Player novoJogador = new Player(nome, novaConexao, stream_entrada, stream_saida, object_entrada, object_saida, partida);
+					
+					//add jogador a lista de jogadores online
+					listaJogadores.add(novoJogador);
+					
+					//envia mensagem para aguardar restante dos jogador
+					stream_saida.writeUTF("Aguardando adversário...");
+					
+					contPlayers++;
+				}
+				catch(IOException ex)
+				{
+					System.out.println("Não foi possível abrir um novo socket!");
+					return;
+				}
+			}
+			
+			//Jogando...
+			//Roda solicitações dos Players
+			rodaPartida(listaJogadores);
+			
+			//Analisa as escolhas dos jogadores
+			analisaPartida(listaJogadores);
+			
+			//CONTINUAR
 		}
 		catch (IOException ex)
 		{
 			System.out.println("Não foi possível abrir o ServerSocket!");
 			return;
 		}
-		
-		while(contPlayers < NUM_MAX_PLAYERS)
-		{
-			try
-			{
-				//abre socket e streams de entrada e saida
-				Socket novaConexao = serverSocket.accept();
-				DataInputStream stream_entrada = new DataInputStream(novaConexao.getInputStream());
-				DataOutputStream stream_saida = new DataOutputStream(novaConexao.getOutputStream());
-				ObjectInputStream object_entrada = new ObjectInputStream(novaConexao.getInputStream());
-				ObjectOutputStream object_saida = new ObjectOutputStream(novaConexao.getOutputStream());
-				
-				//Pedir Nome
-				String nome = stream_entrada.readUTF();
-				
-				//Cria objeto Jogador
-				Player novoJogador = new Player(nome, novaConexao, stream_entrada, stream_saida, object_entrada, object_saida, partida);
-				
-				//add jogador a lista de jogadores online
-				listaJogadores.add(novoJogador);
-				
-				//envia mensagem para aguardar restante dos jogador
-				stream_saida.writeUTF("Aguardando adversário...");
-				
-				contPlayers++;
-			}
-			catch(IOException ex)
-			{
-				System.out.println("Não foi possível abrir um novo socket!");
-				return;
-			}
-		}
-		
-		//Jogando...
-		//Roda solicitaï¿½ï¿½es dos Players
-		rodaPartida(listaJogadores);
-		
-		//Analisa as escolhas dos jogadores
-		analisaPartida(listaJogadores);
-		
-		
 	}
 
 }
